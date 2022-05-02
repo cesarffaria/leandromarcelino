eed93198a5da482a2cf9617f241a25e06e900cac
César Eugénio <cesarffaria@gmail.com>
2022-05-02 17:05:08 +0100

    header mobile block

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,25 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { useQuery } from "@apollo/react-hooks";
+import { Link, NavLink } from "react-router-dom";
 
 import Navigation from "../Navigation";
+
+import logoBlack from "../../assets/logo_black.svg";
+import logoWhite from "../../assets/logo_white.svg";
+import burger from "../../assets/burger.svg";
+import closedBurger from "../../assets/closedBurger.svg";
 
 import HEADER_QUERY from "../../queries/header/header.js";
 
 
+
 const Header = () => {
+  const [menuStatus, setMenuStatus] = useState("closed");
+
   const { loading, error, data } = useQuery(HEADER_QUERY)
 
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error :(</p>
- 
+
 
   const headerAttributes = data.header.data.attributes;
   const headerNome = headerAttributes.Nome;
   const headerLogoURL = process.env.REACT_APP_BACKEND_URL + headerAttributes.Logo.data.attributes.url;
-    
+
+
+
+  const hamburger = () => {
+    //console.log("before: " + menuStatus);
+    menuStatus == "open" ? setMenuStatus("closed") : setMenuStatus("open");
+
+    //console.log("before: " + menuStatus);
+  }
+
   return (
     <div id="header" className="header">
       <Navigation name={headerNome} url={headerLogoURL} />
+      <div className="header-mobile">
+        <NavLink to={{ pathname: "/" }} className="navbar-brand">
+          <img
+            alt={headerNome}
+            src={headerLogoURL}
+            className="d-inline-block align-top"
+          />
+        </NavLink>
+        <p>LEANDRO
+        MARCELINO</p>
+        <div className="burger">
+          <Link to={menuStatus == "closed" ? `/menu` : `/`}>
+            <button className='burger-button' onClick={hamburger}>
+              <img src={menuStatus == "closed" ? burger : closedBurger} />
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
--- frontend/src/pages/Contactos.js
+++ frontend/src/pages/Contactos.js
@@ -28,7 +28,6 @@ const Contactos = () => {
           <Col lg={6}>
             <h1>CONTACTOS</h1>
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d684.1098957425851!2d-8.723840643864307!3d39.65985795374832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd227623ca8344f7%3A0x174079d8958563f7!2s2495-016!5e0!3m2!1spt-PT!2spt!4v1650727527160!5m2!1spt-PT!2spt" width="100%" height="600" allowFullScreen="" referrerPolicy="no-referrer-when-downgrade"></iframe>
-
             <ul>
               <li>
                 Rua Senhora do Monte, Nº30, Casal dos Lobos 2495-016 São Mamede
@@ -43,6 +42,20 @@ const Contactos = () => {
           </Col>
           <Col lg={6}>
             <h1 className="alt-h1">PEÇA UM ORÇAMENTO</h1>
+            <form>
+              <div className="d-flex">
+                <input type="text" name="nome" placeholder="Nome" />
+                <input type="text" name="apelido" placeholder="Apelido" />
+                <input type="text" name="email" placeholder="E-mail" />
+              </div>
+              <div className="d-flex">
+                <textarea type="text" name="mensagem" placeholder="Mensagem" />
+              </div>
+              <input type="checkbox" name="rgpd" /><label for="rgpd">Concordo com a política de privacidade deste site</label>
+              <div className="d-flex">
+                <input type="submit" value="Enviar" />
+              </div>
+            </form>
           </Col>
         </Row>
       </Container>
