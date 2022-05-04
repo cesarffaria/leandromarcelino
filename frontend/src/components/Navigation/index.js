69694499a03d92047e3ff4b5cfed05a6e5781bec
jo-ana <jraimundo@terastudio.pt>
2022-05-04 13:29:33 +0100

    Redes Sociais
    Imagens Redes Sociais no menu Desktop

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -5,6 +5,8 @@ import { useQuery } from "@apollo/react-hooks";
 import {  Navbar, Container, Nav,  } from "react-bootstrap";
 
 import SERVICOS_NOME_QUERY from "../../queries/service/services_names";
+import logoF from "../../assets/facebook-f.svg";
+import logoIN from "../../assets/linkedin-in.svg";
 
 const Navigation = ({ name, url }) => {
 
@@ -20,7 +22,7 @@ const Navigation = ({ name, url }) => {
           { data.servicos.data.map((servico) => (
             <NavLink key={servico.id} to={{pathname: `/service/${servico.attributes.Slug}`}} className="nav-link" >{servico.attributes.Nome}</NavLink>
           ))}
-          <ul className="social-list"><li><a href="https://www.facebook.com/LM-Instala%C3%A7%C3%B5es-Eletricas-110138071688000" target="_blank">f</a></li><li><a href="https://www.linkedin.com/company/lm-instala%C3%A7%C3%B5es-el%C3%A9tricas/" target="_blank">in</a></li></ul>
+          <ul className="social-list"><li><a href="https://www.facebook.com/LM-Instala%C3%A7%C3%B5es-Eletricas-110138071688000" target="_blank"><img src={logoF} alt="F" width="10" height="18"/></a></li><li><a href="https://www.linkedin.com/company/lm-instala%C3%A7%C3%B5es-el%C3%A9tricas/" target="_blank"><img src={logoIN} alt="in" width="20" height="20"/></a></li></ul>
         </Nav>
       </Container>
     </Navbar>
